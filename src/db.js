e0db78d9162a0b3cec50f28b14f5a12036ba5977
arega <arega@4over.com>
2018-10-31 07:23:24 -0700

    cant run dev

---
+++ src/db.js
@@ -0,0 +1,14 @@
+//connect remote prisma allows query with js
+
+const { Prisma } = require('prisma-binding');
+
+
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false
+})
+
+module.exports = db;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({patch: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// handle cookies paopulate current users
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+}, serverReady => {
+    console.log(`Server is now running on port http:/localhost:${ServerReady.port}`)
+});
